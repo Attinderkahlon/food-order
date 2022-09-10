0a2c9e3928487f770908ebd6fd4a31e7ed07ba87
Attinder singh <attinderkahlon@gmail.com>
2022-09-10 16:01:29 +1000

    header component with styling

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import mealsImage from "../../assets/meals.jpg"
+import classes from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <>
+      <header className=''>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt='meals' />
+      </div>
+    </>
+  )
+}
+
+export default Header
