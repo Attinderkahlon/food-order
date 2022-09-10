7d05fc48f6762b9d27f692ff0a4795bae5bd74a0
Attinder singh <attinderkahlon@gmail.com>
2022-09-11 09:17:42 +1000

    updated cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 
@@ -17,13 +18,13 @@ function App() {
   }
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   )
 }
 
